1a3da48a63cba14fce8bcbb007b98b2d1fe99253
vseerapu <vseerapu@gmail.com>
2022-10-12 14:35:50 -0600

    Modified file with Test Case-1

--- src/in/inueron/main/Main.java
+++ src/in/inueron/main/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Java Development");
-		System.out.println("Work hard to become an expert");
+		System.out.println("Connecting to github through eclipse");
 		
 		// format : command + Shit + F
 		// run : control + shift + R 
---
+++ src/in/inueron/main/TestCase1.java
@@ -0,0 +1,9 @@
+package in.inueron.main;
+
+public class TestCase1 {
+	
+	public static void main(String[] args) {
+		System.out.println("This is a modified test case");
+	}
+
+}
